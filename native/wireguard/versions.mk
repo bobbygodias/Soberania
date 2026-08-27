359fccdc353f52a40fe9b96ebaf2e747a1e0ebe8
Bobby Dias - O Capitão <renatogodias@gmail.com>
2026-08-27 16:27:07 -0300

    native: pin WireGuard build inputs

---
+++ native/wireguard/versions.mk
@@ -0,0 +1,8 @@
+# Inputs congelados para o primeiro laboratório nativo.
+# Atualizações exigem revisão explícita e checkpoint.
+
+SOBERANIA_GO_VERSION := 1.24.3
+SOBERANIA_GO_LINUX_AMD64_SHA256 := 3333f6ea53afa971e9078895eaa4ac7204a8c6b5c68c10e6bc9a33e8e391bdd8
+
+# Mesma revisão usada pelo build Android upstream inspecionado em 2026-08-27.
+SOBERANIA_WIREGUARD_GO_VERSION := v0.0.0-20250521234502-f333402bd9cb
